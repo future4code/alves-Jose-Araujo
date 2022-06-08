4148f38bdb1374c8611227941555ae05eb9d1b38
Carlos Araújo <carlosaaraujopc@gmail.com>
2022-06-07 22:50:07 -0300

    Adicionado para styled-components o CardPequeno.js

--- modulo2/props-labedin/labedin/src/components/CardPequeno/CardPequeno.js
+++ modulo2/props-labedin/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,22 +1,22 @@
 import React from "react";
-import "./CardPequeno.css";
+import { SectionCardPequeno } from "./Styles";
 import EmailIcon from "../.././img/envelope.svg";
 import AddressIcon from "../.././img/map-pin.svg";
 
 export function CardPequeno({ email, endereco }) {
 	return (
-		<section>
+		<SectionCardPequeno>
 			<div className="info info-email">
-				<img src={EmailIcon} />
+				<img src={EmailIcon} alt="" />
 				<h4>E-mail:</h4>
 				<p>{email}</p>
 			</div>
 
 			<div className="info info-endereco">
-				<img src={AddressIcon} />
+				<img src={AddressIcon} alt="" />
 				<h4>Endereço:</h4>
 				<p>{endereco}</p>
 			</div>
-		</section>
+		</SectionCardPequeno>
 	);
 }
