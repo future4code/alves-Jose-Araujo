05c29f14ac75f74d1b1ac9c0c8171f655c8bd7d9
Carlos Araújo <carlosaaraujopc@gmail.com>
2022-06-07 22:54:28 -0300

    Adicionado para styled-components o CardGrande.js

--- modulo2/props-labedin/labedin/src/components/CardGrande/CardGrande.js
+++ modulo2/props-labedin/labedin/src/components/CardGrande/CardGrande.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import './CardGrande.css'
+import React from "react";
+import "./CardGrande.css";
+import { BigCardContainer } from "./Styles";
 
 function CardGrande(props) {
-    return (
-        <div className="bigcard-container">
-            <img src={ props.imagem } />
-            <div>
-                <h4>{ props.nome }</h4>
-                <p>{ props.descricao }</p>
-            </div>
-        </div>
-    )
+	return (
+		<BigCardContainer>
+			<div className="bigcard-container">
+				<img src={props.imagem} alt="" />
+				<div>
+					<h4>{props.nome}</h4>
+					<p>{props.descricao}</p>
+				</div>
+			</div>
+		</BigCardContainer>
+	);
 }
 
 export default CardGrande;
