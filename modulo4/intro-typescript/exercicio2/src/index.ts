d66aaefe92dba5180c8abfbb8ae45a41f0bea1ab
Carlos Araújo <carlosaaraujopc@gmail.com>
2022-08-10 14:39:58 -0300

    Alteração no retorno da função do exercício 2

--- modulo4/intro-typescript/exercicio2/src/index.ts
+++ modulo4/intro-typescript/exercicio2/src/index.ts
@@ -2,8 +2,8 @@ function imprimeTresCoresFavoritas(
 	cor1: string,
 	cor2: string,
 	cor3: string
-): void {
-	console.log([cor1, cor2, cor3]);
+): string[] {
+	return [cor1, cor2, cor3];
 }
 
 imprimeTresCoresFavoritas("rosa", "vermelho", "marrom");
