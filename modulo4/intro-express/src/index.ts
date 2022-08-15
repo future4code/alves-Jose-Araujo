1d1c55d1b7746b20d327ed3adb1e0d0274ee93fb
Carlos Araújo <carlosaaraujopc@gmail.com>
2022-08-15 15:56:56 -0300

    Exercício 1, 2, 3 e 4 entregues

---
+++ modulo4/intro-express/src/index.ts
@@ -0,0 +1,25 @@
+import express, {
+	application,
+	Request,
+	response,
+	Response,
+} from "express";
+import cors from "cors";
+import { users } from "./data";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3003, () => {
+	console.log("Server is running in http://localhost:3003");
+});
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Olá mundo! esse é o meu primeiro endpoint");
+});
+
+app.get("/users", (req: Request, res: Response) => {
+	res.send(users);
+});
